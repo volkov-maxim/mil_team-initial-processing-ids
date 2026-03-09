ef9abaebc44e57486a0f5ae31237b27aa2ef0d07
Maxim Volkov <vmj.ne4@gmail.com>
2026-03-09 13:19:47 +0300

    style: Fix indentation in __init__.py

--- src/app/validation/__init__.py
+++ src/app/validation/__init__.py
@@ -4,6 +4,6 @@ from app.validation.field_validators import FieldValidationResult
 from app.validation.field_validators import FieldValidators
 
 __all__ = [
-	"FieldValidationResult",
-	"FieldValidators",
+    "FieldValidationResult",
+    "FieldValidators",
 ]
